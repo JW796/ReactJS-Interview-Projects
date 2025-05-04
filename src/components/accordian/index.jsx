7b873c7d73c74da37420799943fc29b1463099f5
James Joseph <98562132+JW796@users.noreply.github.com>
2025-05-03 21:03:12 -0400

    WIP Multi Selection

--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -1,34 +1,42 @@
 import { useState } from "react";
 import data from "./Data";
-import './styles.css';
+import "./styles.css";
 // Single selection
 
 // Multiple selection
 
 export default function () {
   const [selected, setSelected] = useState(null);
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multiple, setMultiple] = useState([]);
 
-  function handleSingleSelection(getCurrentID){
+  function handleSingleSelection(getCurrentID) {
     setSelected(getCurrentID === selected ? null : getCurrentID);
   }
+
+//   function handleMultiSelection() {
+
+//   }
 
   console.log(selected);
   return (
     <div className="wrapper">
+      <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>Enable Multi Selection</button>
+
       <div className="accordian">
         {data && data.length > 0 ? (
           data.map((dataItem) => (
             <div className="item">
-              <div onClick={()=> handleSingleSelection(dataItem.id)}
-              className="title">
+              <div
+                onClick={() => handleSingleSelection(dataItem.id)}
+                className="title"
+              >
                 <h3>{dataItem.question}</h3>
                 <span>+</span>
               </div>
-              {
-                selected === dataItem.id ?
+              {selected === dataItem.id ? (
                 <div className="content">{dataItem.answer}</div>
-                : null
-              }
+              ) : null}
             </div>
           ))
         ) : (
