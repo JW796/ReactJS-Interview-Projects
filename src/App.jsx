1eb79850ee75e21323c10186d2ffcff4b663b52a
James Joseph <98562132+JW796@users.noreply.github.com>
2025-05-13 10:57:12 -0400

    WIP Image Slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
-import { useState } from 'react'
-import './App.css'
-import Accordian from './components/accordian'
-import RandomColor from './components/random color'
-import StarRating from './components/star-rating'
+import { useState } from "react";
+import "./App.css";
+import Accordian from "./components/accordian";
+import RandomColor from "./components/random color";
+import StarRating from "./components/star-rating";
+import ImageSlider from "./components/image-slider";
 
 function App() {
   return (
     <>
       <div className="App">
-      {/* Accordian component */}
-      <Accordian />
+        {/* Accordian component */}
+        <Accordian />
 
-      {/* Random Color Component */}
-      <RandomColor />
+        {/* Random Color Component */}
+        <RandomColor />
 
-      {/* Star Rating */}
-      <StarRating noOfStars={10} />
-        
+        {/* Star Rating */}
+        <StarRating noOfStars={10} />
+
+        {/* Image Slider */}
+        <ImageSlider
+          url="https://picsum.photos/v2/list"
+          page={"1"}
+          limit={"10"}
+        />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/image-slider/index.jsx
+++ src/components/image-slider/index.jsx
@@ -1,23 +1,68 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
 
-export  default function ImageSlider ({url, limit}) {
+export default function ImageSlider({ url, limit = 5, page = 1 }) {
+  const [images, setImages] = useState(0);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-    const [images, setImages] = useState(0);
-    const [currentSlide, setCurrentSlide] = useState(0);
+  async function fetchImages(getUrl) {
+    try {
+      setLoading(true);
 
-    async function fetchImages(getUrl) {
-        try{
-            const response = await fetch(getUrl);
-            const data = await response.json();
+      const response = await fetch(`${getUrl}?page=${page}&limit=${limit}`);
+      const data = await response.json();
 
-        } catch(e) {
-            setErrorMsg(e.message);
-        }
+      if (data) {
+        setImages(data);
+        setLoading(false);
+      }
+    } catch (e) {
+      setErrorMsg(e.message);
+      setLoading(false);
     }
+  }
 
-    useEffect(() =>{
-        if(url !== '') fetchImages(url)
-    }, [url])
+  useEffect(() => {
+    if (url !== "") fetchImages(url);
+  }, [url]);
 
-    return <div className="container"></div>
+  console.log(images);
+
+  if (loading) {
+    return <div>Loading Data ! Please wait</div>;
+  }
+
+  if (errorMsg !== null) {
+    return <div>Error Occurred ! {errorMsg}</div>;
+  }
+
+  return (
+    <div className="container">
+      <BsArrowLeftCircleFill className="arrow arrow-left" />
+      {images && images.length
+        ? images.map((imageItem) => (
+            <img
+              key={imageItem.id}
+              alt={imageItem.download_url}
+              src={imageItem.download_url}
+              className="currnet-image"
+            />
+          ))
+        : null}
+        <BsArrowRightCircleFill className="arrow arrow-right"/>
+        <span className="circle-indicators">
+            {
+                images && images.length ?
+                images.map ((_, index)=> <button
+                key={index}
+                className="current-indicator"
+                >
+                </button>)
+                : null
+            }
+        </span>
+    </div>
+  );
 }
