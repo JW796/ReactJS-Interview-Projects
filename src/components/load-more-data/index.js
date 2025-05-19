06eb4e9c0b210f95a63e3f6adbc04b0f2349009c
James Joseph <98562132+JW796@users.noreply.github.com>
2025-05-19 18:19:09 -0400

    Product Page Setup

--- src/components/load-more-data/index.js
+++ src/components/load-more-data/index.js
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
 
 
 export default function LoadMoreData() {
-    return <div className="container">
 
-    </div>
+    const [loading, setLoading] = useState(false);
+    const [products, setProducts] = useState([]);
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        async function fetchProducts() {
+            try{
+                const response = await fetch(
+                  "https://dummyjson.com/products?limit=20&skip=${count === 0 ? 0 : count * 20}"
+                );
+
+                const result = await response.json();
+            }catch(e){
+                console.log(e);
+            }
+        }
+    }, []);
+
+
+    return <div className="container">
+        {/* https://dummyjson.com/products */}
+        </div>;
 }
