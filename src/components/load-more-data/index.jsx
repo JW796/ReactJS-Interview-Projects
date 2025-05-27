711d995bb33717cfb21a85ee2e39ee73fe9ab0d0
James Joseph <98562132+JW796@users.noreply.github.com>
2025-05-26 20:54:05 -0400

    WIP 2 Load More Products

--- src/components/load-more-data/index.jsx
+++ src/components/load-more-data/index.jsx
@@ -1,49 +1,55 @@
 import { useEffect, useState } from "react";
 
-
 export default function LoadMoreData() {
+  const [loading, setLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [count, setCount] = useState(0);
 
-    const [loading, setLoading] = useState(false);
-    const [products, setProducts] = useState([]);
-    const [count, setCount] = useState(0);
+  async function fetchProducts() {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        `https://dummyjson.com/products?limit=20&skip=${
+          count === 0 ? 0 : count * 20
+        }`
+      );
 
-    
-        async function fetchProducts() {
-            try{
-                setLoading(true)
-                const response = await fetch(
-                  `https://dummyjson.com/products?limit=20&skip=${count === 0 ? 0 : count * 20}`
-                );
+      const result = await response.json();
 
-                const result = await response.json();
+      if (result && result.products && result.products.length) {
+        setProducts(result.products);
+        setLoading(false);
+      }
 
-                if(result && result.products && result.products.length) {
-                    setProducts(result.products)
-                    setLoading(false);
-                }
+      console.log(result);
+    } catch (e) {
+      console.log(e);
+      setLoading(false);
+    }
+  }
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
-                console.log(result);
-            }catch(e){
-                console.log(e);
-                setLoading(false);
-            }
-        }
-        useEffect(() => {
-            fetchProducts();
-        }, []);
+  if (loading) {
+    return <div>Loading data ! Please wait.</div>;
+  }
 
-        if(loading) {
-            return <div>Loading data ! Please wait.</div>
-        }
-
-
-    return <div className="container">
-        <div>
-            {
-                products && products.length ?
-                products.map(item=> <div> </div>) 
-                : null 
-            }
-        </div>
-    </div>;
+  return (
+    <div className="container">
+      <div className="product-container">
+        {products && products.length
+          ? products.map((item) => (
+              <div className="products" key={item.id}>
+                <img src={item.thumbnail} alt={item.title} />
+                <p>{item.title}</p>
+              </div>
+            ))
+          : null}
+      </div>
+      <div className="button-container">
+        <button>Load More Products</button>
+      </div>
+    </div>
+  );
 }
