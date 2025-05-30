ac19766cebb62b64f0fc60fbb89bcf95981436f6
James Joseph <98562132+JW796@users.noreply.github.com>
2025-05-30 14:59:49 -0400

    Functionality + Styling update

--- src/components/tree-view/menu-item.jsx
+++ src/components/tree-view/menu-item.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import MenuList from "./menu-list";
+import {FaMinus, FaPlus} from "react-icons/fa"
 
 export default function MenuItem({ item }) {
   const [displayCurrentChildren, setDisplayCurrentChildren] = useState({});
@@ -13,12 +14,23 @@ export default function MenuItem({ item }) {
 
   return (
     <li>
-      <div style={{disply: 'flex', gap: '20px' }}>
+      <div className="menu-item">
         <p>{item.label}</p>
-        {item && item.children && item.children.length ? <span onClick={()=>handleToggleChildren(item.label)}>+</span> : null}
+        {item && item.children && item.children.length ? (
+          <span onClick={() => handleToggleChildren(item.label)}>
+            {displayCurrentChildren[item.label] ? (
+              <FaMinus color="#fff" size={25} />
+            ) : (
+              <FaPlus color="#fff" size={25} />
+            )}
+          </span>
+        ) : null}
       </div>
 
-      {item && item.children && item.children.length > 0 && displayCurrentChildren[item.label] ? (
+      {item &&
+      item.children &&
+      item.children.length > 0 &&
+      displayCurrentChildren[item.label] ? (
         <MenuList list={item.children} />
       ) : null}
     </li>
